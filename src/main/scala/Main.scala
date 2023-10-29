e4b348a08d0f18bbbaad2c64904e9947c3d88aa8
Simon Ferns <122154257+AdonisCodes@users.noreply.github.com>
2023-10-29 04:45:36 +0200

    Got server setup. Need to run linux firefox to test, isolation is great

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,7 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Await}
 import scala.util.{Failure, Success}
 
 object Main {
@@ -25,20 +25,29 @@ object Main {
     }
 
     val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    val serverBinding = bindingFuture.map { binding =>
+      println(
+        s"Server online at http://${binding.localAddress.getHostString}:${binding.localAddress.getPort}/"
+      )
+      binding
+    }
 
-    bindingFuture.onComplete {
+    serverBinding.onComplete {
       case Success(binding) =>
-        println(
-          s"Server online at http://${binding.localAddress.getHostString}:${binding.localAddress.getPort}/"
-        )
         sys.addShutdownHook {
-            bindingFuture
-                .flatMap(_.unbind())
-                .onComplete(_ => actorSystem.terminate())
+          bindingFuture
+            .flatMap(_.unbind())
+            .onComplete(_ => actorSystem.terminate())
         }
       case Failure(ex) =>
         println(s"Server could not start: ${ex.getMessage}")
         actorSystem.terminate()
     }
+
+    Await.result(
+      actorSystem.whenTerminated,
+      scala.concurrent.duration.Duration.Inf
+    )
   }
+
 }
